fcc7b86d4f9d80fd04239eeaa063d01c0b004a70
Alen Rubilar <lclc.alen@gmail.com>
2022-10-16 02:24:47 -0700

    Model actually learns

--- ml/main.py
+++ ml/main.py
@@ -1,81 +1,70 @@
-# %%
 import pandas
 import numpy
-import csv
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
-
-from random import shuffle
 
 
-# %%
-table = list([float(_) for _ in row] for row in csv.reader(open("ecg.csv", "r")))
-normal = list(map(lambda row: row[:-1], filter(lambda row: row[-1] == 0, table)))
-abnormal = list(map(lambda row: row[:-1], filter(lambda row: row[-1] == 1, table)))
-dataset = [*[(0, _) for _ in normal], *[(1, _) for _ in abnormal]]
-shuffle(dataset)
+SIZE = 100
+EPOCHS = 25
 
 
-# %%
-class Block(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Block, self).__init__()
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size) -> None:
+        super().__init__()
+
+        self.input_size = input_size
         self.hidden_size = hidden_size
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size).float()
-        self.c2o = nn.Linear(input_size + hidden_size, output_size).float()
+        self.output_size = output_size
 
-    def forward(self, x, hidden_state):
-        combined = torch.cat((x, hidden_state), 1)
-        hidden = torch.sigmoid(self.i2h(combined.float()))
-        output = self.c2o(combined)
-        return output, hidden
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, h):
+        y_hat = self.l1(
+            x,
+        )
+        h = torch.sigmoid(y_hat)
+        y_hat = self.l2(h)
+        y_hat = torch.sigmoid(y_hat)
+        return y_hat, h
 
     def init_hidden(self):
-        return nn.init.kaiming_uniform_(torch.empty(1, self.hidden_size))
+        return torch.zeros(1, self.hidden_size, dtype=torch.float32)
 
 
-# %%
-def ResampleLinear1D(original, targetLen):
-    original = numpy.array(original, dtype=numpy.float32)
-    index_arr = numpy.linspace(0, len(original) - 1, num=targetLen, dtype=numpy.float32)
-    index_floor = numpy.array(index_arr, dtype=numpy.int64)  # Round down
+def ResampleLinear1D(x, target_size):
+    x = numpy.array(x)
+    index_array = numpy.linspace(0, len(x) - 1, num=target_size)
+    index_floor = numpy.array(index_array, dtype=numpy.int64)
     index_ceil = index_floor + 1
-    index_rem = index_arr - index_floor  # Remain
-    val1 = original[index_floor]
-    val2 = original[index_ceil % len(original)]
-    interp = val1 * (1.0 - index_rem) + val2 * index_rem
-    assert len(interp) == targetLen
-    return interp
+    index_remaining = index_array - index_floor
+    lower = x[index_floor]
+    upper = x[index_ceil % len(x)]
+    interpolation = lower * (1.0 - index_remaining) + upper * index_remaining
+    return interpolation
 
 
-# %%
-EPOCHS = 1
-RESAMPLING_SIZE = 100
-
-model = Block(RESAMPLING_SIZE, RESAMPLING_SIZE, 1)
-criterion = nn.CrossEntropyLoss()
+dataframe = pandas.read_csv("ecg.csv")
+model = Model(SIZE, SIZE, 1)
+criterion = nn.BCELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 
+i = 0
 for epoch in range(EPOCHS):
-    running_loss = 0.0
-    hidden_state = model.init_hidden()
-    for i, (label, inputs) in enumerate(dataset):
-        print(label)
+    h = model.init_hidden()
+    for _, (*x, y) in dataframe.iterrows():
+        x = ResampleLinear1D(x, SIZE)
+        x = torch.tensor(x, dtype=torch.float32)
+        y = torch.tensor(y).unsqueeze(0)
+        y_hat, h = model(x, h)
+        loss = criterion(y_hat, y)
 
-        inputs = ResampleLinear1D(inputs, RESAMPLING_SIZE)
-        optimizer.zero_grad()
-        outputs, hidden_state = model(torch.tensor(inputs).reshape(1, RESAMPLING_SIZE), hidden_state)
-        loss = criterion(outputs, label)
+        if i % 1000 == 0:
+            print(loss)
+
         loss.backward()
         optimizer.step()
-        running_loss += loss.item()
-        print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
-
-
-# %%
-
-
-
+        optimizer.zero_grad()
+        i += 1
